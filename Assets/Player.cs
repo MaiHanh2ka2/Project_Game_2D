f4036f3f9e555516da0aff229e671e1a6e915685
Ngo Tien Dung <91784385+MaiHanh2ka2@users.noreply.github.com>
2024-04-17 13:51:16 +0700

    Animate run and idle

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,29 +4,91 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("Move info")]
     public float moveSpeed;
-    public Rigidbody2D rb;
     public float jumpForce;
+
+    private bool canDoubleJump = true;
+
 
     private float movingInput;
 
+    [Header("Collision info")]
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    private bool isGrounded;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+
+
+        CollisionChecks();
+        InputChecks();
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        Move();
+    }
+
+    private void InputChecks()
+    {
         movingInput = Input.GetAxisRaw("Horizontal");
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            JumpButton();
+
         }
-        
-        rb.velocity = new Vector2(moveSpeed * movingInput , rb.velocity.y);
     }
+
+    private void JumpButton()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+        else if (canDoubleJump)
+        {
+            canDoubleJump = false;
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y * groundCheckDistance));
+    }
+
+
+    
 }
