21dce0e449758748cef6a93831e8fdd8e954b9a0
Ngo Tien Dung <91784385+MaiHanh2ka2@users.noreply.github.com>
2024-04-19 11:49:07 +0700

    Fire trap - part 1

---
+++ Assets/Trap.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        Debug.Log("Knocked!");
+    }
+}
