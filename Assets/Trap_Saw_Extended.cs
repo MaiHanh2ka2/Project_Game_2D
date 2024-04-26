95b3d56c7fdd57583f9d5cde725c8e340ef83cf8
Ngo Tien Dung <91784385+MaiHanh2ka2@users.noreply.github.com>
2024-04-27 02:00:21 +0700

    Saw trap with extended travel points

--- Assets/Trap_Saw_Extended.cs
+++ Assets/Trap_Saw_Extended.cs
@@ -2,43 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap_Saw : Trap
+public class Trap_Saw_Extended : Trap
 {
-
     private Animator anim;
 
     [SerializeField] private Transform[] movePoint;
     [SerializeField] private float speed;
-    [SerializeField] private float cooldown;
 
     private int movePointIndex;
-    private float cooldownTimer;
+    private bool goingForward = true;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        anim.SetBool("isWorking", true);
         transform.position = movePoint[0].position;
+
+        Flip();
     }
 
-    
+
     void Update()
     {
-        cooldownTimer -= Time.deltaTime;
-
-        bool isWorking = cooldownTimer < 0;
-        anim.SetBool("isWorking", isWorking);
-
-        if (isWorking)
-        transform.position = Vector3.MoveTowards(transform.position, movePoint[movePointIndex].position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, movePoint[movePointIndex].position, speed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, movePoint[movePointIndex].position) < 0.15f)
         {
-            Flip();
-            cooldownTimer = cooldown;
-            movePointIndex++;
+            if (movePointIndex == 0)
+            {
+                Flip();
+                goingForward = true;
+            }
+
+            if (goingForward)
+                movePointIndex++;
+            else
+                movePointIndex--;
 
             if (movePointIndex >= movePoint.Length)
-                movePointIndex = 0;
+            {
+                movePointIndex = movePoint.Length - 1;
+                goingForward = false;
+                Flip();
+            }
 
         }
     }
